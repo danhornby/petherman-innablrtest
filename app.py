91cc40fbfc314e4b6b80fe44d3ba0aed44fde98c
petherman <vasinpether@gmail.com>
2021-10-07 13:43:19 +1100

    cleaning up the code and adding more comments

--- app.py
+++ app.py
@@ -5,23 +5,24 @@ import json
 
 app = Flask(__name__)
 
-#
+# homepage route
 @app.route('/')
 def hello_world():
     return 'This is the homepage'
-    
+
+# status endpoint    
 @app.route('/status')
 def status():
     last_commit_sha=os.environ['COMMIT_SHA']
-    
     # for local testing,
     #if not last_commit_sha:
     #    last_commit_sha="abc1234567"
     
     #forcefulling failing test
-    last_commit_sha="12345"
+    # see build 51 for failure result
+    #last_commit_sha="12345"
     
-            
+    # building out json file for result
     json_build = {
         "my-application": [
             {
